4ac887563d38762938b8427036bf42246cf74124
suras <msurendar@outlook.com>
2013-06-11 12:19:05 +0530

    initial chat app

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,20 +2,29 @@ class ChatsController < ApplicationController
   
   
   def index
-    
+    @users = User.find(:all); 
     
   end
   
 
 
   def create
+ if  params[:user].present?
+    
+    @channel = "/messages/private/#{params[:user]}"
+    @sender_channel = "/messages/private/#{current_user.name}"
+    @message = { :name => current_user.name, :msg => params[:message] }
+   
+  else
+   
     @channel = "/messages/public"
-    @message = { :msg => params[:message] }
+    @message = { :username => session[:username], :msg => params[:message] }
      respond_to do |format|
     format.js
+   end
   end
     
-  end
+ end
 
 
 end
